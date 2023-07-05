a6a547c3f3ba713d76d893908e760a6777076f89
mkpatel-247 <meet.codes247@gmail.com>
2023-07-05 16:26:13 +0530

    Day06 Task Build Number Counter Completed

---
+++ Day 06/main.js
@@ -0,0 +1,19 @@
+const counter = document.getElementById('counter');
+const reset = document.querySelector('.btn-reset');
+const increase = document.querySelector('.increase');
+const decrease = document.querySelector('.decrease');
+
+let count = 0;
+increase.addEventListener('click', () => {
+    count++;
+    counter.innerHTML = count;
+})
+decrease.addEventListener('click', () => {
+    if (count > 0)
+        count--;
+    counter.innerHTML = count;
+})
+
+reset.addEventListener('click', () => {
+    counter.innerHTML = 0;
+})
